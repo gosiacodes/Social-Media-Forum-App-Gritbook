588d779ce500ef263cb07b5d4d8fb1c899a81131
Gosia <margareta.xx@gmail.com>
2022-03-28 15:51:07 +0200

    Add sidebar with functionality and displayHandler.ts file
    Add sidebar in index and styling in CSS + functionality in main.ts and in displayHandler.ts + update docs.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { onValue, ref } from "firebase/database";
 import { db } from "./modules/firebaseApp";
 import { Loginhandler } from "./modules/LoginHandler";
+import { showForum } from "./modules/displayHandler"
 
 // Created database and template objects
 const dbRefMess = ref(db, '/messages');
 const dbRefUsers = ref(db, '/users');
+const forumButtons = document.querySelectorAll(".forum-btn");
 
 onValue(dbRefMess, (snapshot) => {
   const messagesData = snapshot.val();
@@ -17,3 +19,12 @@ onValue(dbRefUsers, (snapshot) => {
 });
 
 new Loginhandler();
+
+forumButtons.forEach((btn) => {
+    btn.addEventListener("click", (event:Event) => {
+        console.log('forum button clicked');
+        const target = event.target as HTMLElement;
+        const forum:string = target.id;
+        showForum(forum);
+    });
+});
