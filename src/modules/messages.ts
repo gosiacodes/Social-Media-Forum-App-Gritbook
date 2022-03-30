c0a8fdbcf25f794cad26b9e212eda9b550df6ae2
Othmaann <91528006+Othmaann@users.noreply.github.com>
2022-03-30 12:19:30 +0200

    small changes

--- src/modules/messages.ts
+++ src/modules/messages.ts
@@ -2,30 +2,15 @@ import { db } from "./firebaseApp";
 import { ref, remove, update } from "firebase/database";
 
 //posts class med constructor
-class posts {
+class Message {
     constructor(
         public readonly id: string,
         public readonly username: string,
-        public message: string,
-        private readonly userId: string
+        private readonly timestamp: string,
+        public message: string
     ) { }
-    public displayChat(): void {
-
-        const userDiv: HTMLDivElement = document.createElement('div');
-        const messDiv: HTMLDivElement = document.createElement('div');
-        const usernameEl: HTMLHeadingElement = document.createElement('h5');
-        const messageEl: HTMLParagraphElement = document.createElement('p');
-        const delButton: HTMLButtonElement = document.createElement('button');
-        const messContainer: HTMLDivElement = document.createElement('div');
-
-        usernameEl.innerText = this.username;
-        messageEl.innerText = this.message;
-        delButton.innerText = 'X';
-        delButton.className = 'delete-btn';
-        userDiv.className = 'user-div';
-        messDiv.className = 'mess-div';
-        messContainer.className = 'message-container';
-        messContainer.id = this.id;
-
+    public clearChat(): void {
+        document.querySelector(`#${this.id}`).remove();
     }
 }
+
