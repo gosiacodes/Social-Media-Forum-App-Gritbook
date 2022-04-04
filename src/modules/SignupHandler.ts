f45e88895a9bb4c804a900a061f7801223fe008f
ArbieTech <arbie2000@gmail.com>
2022-04-04 13:18:30 +0200

    signup-feat-update

--- src/modules/SignupHandler.ts
+++ src/modules/SignupHandler.ts
@@ -1,6 +1,7 @@
 import { User } from "./User";
 import { db, fetchUsersData } from "./firebaseApp";
 import { push, ref, update } from "firebase/database";
+import { showForum } from "./displayHandler";
 
 export class SignupHandler {
     private signInForm: HTMLFormElement;
@@ -43,8 +44,7 @@ export class SignupHandler {
             alert('password does not match');
             return;
         } else {
-            alert('signup success');
-            this.initNewUser(new User(this.usernameInField.value, this.passwordInField.value));
+            this.signupUser(new User(this.usernameInField.value, this.passwordInField.value));
         }
     }
 
@@ -55,8 +55,8 @@ export class SignupHandler {
         this.signInForm.style.display = "flex";
     }
 
-    initNewUser(user: User) {
-        console.log('initNewUser invoked');
+    signupUser(user: User) {
+        alert('signup success');
         // Create new user-object
         const userToAdd = {
             username: user.getUsername(),
@@ -70,8 +70,31 @@ export class SignupHandler {
         newUser[key] = userToAdd;
 
         update(dbRefUsers, newUser);
+        this.login(user.getUsername(), key);
         this.clearForm();
     }
+
+    login(username: string, key: string) {
+        const profileDiv: HTMLDivElement = document.querySelector('.profile');
+        const forumSection: HTMLDivElement = document.querySelector('#forums-section');
+        // const loginForm: HTMLDivElement = document.querySelector('#login-form');
+        const signUpForm: HTMLDivElement = document.querySelector('#signup-form');
+        const logoutBtn: HTMLDivElement = document.querySelector('#logout-btn');
+        const headerData:HTMLDivElement = document.querySelector('#header-data');
+        
+        console.log('login success');
+        const userItem = document.querySelector('.user');
+        userItem.textContent = username;
+        userItem.setAttribute('id', key);
+        this.clearForm();
+        showForum('travel-forum');
+    
+        headerData.style.justifyContent = 'space-between';
+        profileDiv.style.display = "flex";
+        forumSection.style.display = "flex";
+        signUpForm.style.display = "none";
+        logoutBtn.style.display = "inline-block";
+      }
 
     clearForm() {
         this.signUpform.reset();
