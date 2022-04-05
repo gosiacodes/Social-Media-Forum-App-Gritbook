f1255ca3646a74412ef8f76caf7ccac6a7efff65
Othmaann <91528006+Othmaann@users.noreply.github.com>
2022-04-05 11:47:25 +0200

    .

--- src/modules/userprofile.ts
+++ src/modules/userprofile.ts
@@ -1,3 +1,9 @@
 import { db } from "./firebaseApp";
 import { onValue, ref, get } from "firebase/database";
 import { User } from "./User";
+<<<<<<< Updated upstream
+=======
+
+const Name = sessionStorage.getItem('username');
+
+>>>>>>> Stashed changes
